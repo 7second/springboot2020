0e701d15ba54cf14626d58f7af522e5a9678c8fc
7second <2318787691@qq.com>
2020-05-12 23:28:39 +0800

    hello-world

---
+++ springboot-helloworld/src/test/java/com/yupp/boot/SpringbootHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yupp.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
