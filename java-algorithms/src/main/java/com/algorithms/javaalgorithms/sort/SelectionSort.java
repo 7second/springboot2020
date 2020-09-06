1697306de22e0d635e49200cd1068d1e77d7db4c
7second <2318787691@qq.com>
2020-09-06 22:48:20 +0800

    选择排序

---
+++ java-algorithms/src/main/java/com/algorithms/javaalgorithms/sort/SelectionSort.java
@@ -0,0 +1,38 @@
+package com.algorithms.javaalgorithms.sort;
+
+import java.util.Arrays;
+
+/**
+ * @author yupanpan
+ * @date 2020/9/6 22:36
+ * @desc 选择排序
+ */
+public class SelectionSort {
+
+    public int[] sort(int[] sourceArray) {
+        int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
+        // 总共要经过n-1次比较
+        for (int i = 0; i < arr.length-1; i++) {
+            int min = i;
+
+            // 每轮需要比较次数n-i
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[j]<arr[min]) {
+                    // 记录目前能找到的最小值元素的下标
+                    min = j;
+                }
+            }
+
+            // 将找到的最小值和i位置所在的值进行交换
+            if (i!=min){
+                int temp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = temp;
+            }
+        }
+
+        return arr;
+
+    }
+
+}
